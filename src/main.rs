e6fb1e03de3977f0fe6a6d615c3a8aa40c482273
ATEUFO <ateufoarthur@gmail.com>
2026-06-19 20:23:28 +0100

    Ajout de la variable s et lecture de l'entrée utilisateur dans la fonction main

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-
+use std::io;
 fn main() {
-    let mut n: i32 =5;
+    let mut n: i32 = 5;
+    let mut s: String = String::new();
     println!("Hello, world!");
     println!("La valeur de n est {}", n);
-    n=n+10;
+    n = n + 10;
     println!("La valeur de n est {}", n);
+
+    io::stdin().read_line(&mut s).expect("erreur de lecture");
+    println!("La valeur de s est {}", s);
 }
